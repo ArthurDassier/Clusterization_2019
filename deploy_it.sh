eac2d8be2772c490c2ca4c14e8bee13debf9c07e
Arthur Dassier <arthur.dassier@epitech.eu>
2019-12-04 13:40:14 +0100

    Try DaemonSet + result is working alone

--- deploy_it.sh
+++ deploy_it.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
+echo -e "Scheduling cadvisor on all nodes ...\n"
+
+# kubectl --kubeconfig="dopclusterization2019-kubeconfig.yaml" apply -f cadvisor.daemonset.yaml
+
 echo -e "Creating poll ...\n"
 
-kubectl --kubeconfig="dopclusterization2019-kubeconfig.yaml" apply -f poll.deployment.yaml
-kubectl --kubeconfig="dopclusterization2019-kubeconfig.yaml" apply -f poll.service.yaml
-kubectl --kubeconfig="dopclusterization2019-kubeconfig.yaml" apply -f poll.ingress.yaml
+# kubectl --kubeconfig="dopclusterization2019-kubeconfig.yaml" apply -f poll.deployment.yaml
+# kubectl --kubeconfig="dopclusterization2019-kubeconfig.yaml" apply -f poll.service.yaml
+# kubectl --kubeconfig="dopclusterization2019-kubeconfig.yaml" apply -f poll.ingress.yaml
+
+echo -e "\nCreating result ...\n"
+
+kubectl --kubeconfig="dopclusterization2019-kubeconfig.yaml" apply -f result.deployment.yaml
+kubectl --kubeconfig="dopclusterization2019-kubeconfig.yaml" apply -f result.service.yaml
+kubectl --kubeconfig="dopclusterization2019-kubeconfig.yaml" apply -f result.ingress.yaml
 
 
-echo -e "Creating redis ...\n"
+echo -e "\nCreating redis ...\n"
 
 kubectl --kubeconfig="dopclusterization2019-kubeconfig.yaml" apply -f redis.configmap.yaml
 kubectl --kubeconfig="dopclusterization2019-kubeconfig.yaml" apply -f redis.deployment.yaml
