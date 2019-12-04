b8d5b44c15105382bdf94a1e81d51429fbc04cb0
Arthur Dassier <arthur.dassier@epitech.eu>
2019-12-04 11:33:05 +0100

    Anti affinity

--- deploy_it.sh
+++ deploy_it.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-echo "Creating poll ..."
+echo -e "Creating poll ...\n"
 
 kubectl --kubeconfig="dopclusterization2019-kubeconfig.yaml" apply -f poll.deployment.yaml
 kubectl --kubeconfig="dopclusterization2019-kubeconfig.yaml" apply -f poll.service.yaml
 kubectl --kubeconfig="dopclusterization2019-kubeconfig.yaml" apply -f poll.ingress.yaml
 
 
-echo "Creating redis ..."
+echo -e "Creating redis ...\n"
 
 kubectl --kubeconfig="dopclusterization2019-kubeconfig.yaml" apply -f redis.configmap.yaml
 kubectl --kubeconfig="dopclusterization2019-kubeconfig.yaml" apply -f redis.deployment.yaml
